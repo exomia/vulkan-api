b0369bd4522818d36a56f9a3e169daf4eaad2f5d
Facebamm <dbaumertffg@web.de>
2021-01-22 23:18:16 +0100

    version upgrade 1.2.166

--- src/Exomia.Vulkan.Api.Core/Vk.Const.cs
+++ src/Exomia.Vulkan.Api.Core/Vk.Const.cs
@@ -15,7 +15,7 @@ namespace Exomia.Vulkan.Api.Core
 {
     public static partial class Vk
     {
-        public const int    VK_HEADER_VERSION                                        = 165;
+        public const int    VK_HEADER_VERSION                                        = 166;
         public const int    VK_NULL_HANDLE                                           = 0;
         public const uint   VK_ATTACHMENT_UNUSED                                     = ~0U;
         public const uint   VK_FALSE                                                 = 0U;
