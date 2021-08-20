ddbeb8b6f5af7b5ac0f4455695ed9a511c6cd157
Daniel Bätz <baetz.daniel@gmail.com>
2021-08-20 13:59:08 +0200

    added operators to VkDeviceSize

--- src/Exomia.Vulkan.Api.Core/Vk.Structs.Typedefs.cs
+++ src/Exomia.Vulkan.Api.Core/Vk.Structs.Typedefs.cs
@@ -8,6 +8,8 @@
 
 #endregion
 
+using System.Runtime.CompilerServices;
+
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
 // ReSharper disable UnusedMember.Global
@@ -19,6 +21,7 @@ namespace Exomia.Vulkan.Api.Core
         private readonly ulong _address;
 #pragma warning restore 649
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static implicit operator VkDeviceAddress(ulong address)
         {
             VkDeviceAddress value;
@@ -26,36 +29,43 @@ namespace Exomia.Vulkan.Api.Core
             return value;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator ==(VkDeviceAddress left, VkDeviceAddress right)
         {
             return left._address == right._address;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator !=(VkDeviceAddress left, VkDeviceAddress right)
         {
             return left._address != right._address;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool Equals(in VkDeviceAddress obj)
         {
             return obj._address == _address;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override bool Equals(object? obj)
         {
             return obj is VkDeviceAddress vkDeviceAddress && Equals(in vkDeviceAddress);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override int GetHashCode()
         {
             return _address.GetHashCode();
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static implicit operator ulong(VkDeviceAddress value)
         {
             return value._address;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override string ToString()
         {
             return _address.ToString();
@@ -68,6 +78,7 @@ namespace Exomia.Vulkan.Api.Core
         private readonly ulong _size;
 #pragma warning restore 649
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static implicit operator VkDeviceSize(ulong size)
         {
             VkDeviceSize value;
@@ -75,36 +86,85 @@ namespace Exomia.Vulkan.Api.Core
             return value;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator ==(VkDeviceSize left, VkDeviceSize right)
         {
             return left._size == right._size;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool operator !=(VkDeviceSize left, VkDeviceSize right)
         {
             return left._size != right._size;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator >=(VkDeviceSize left, VkDeviceSize right)
+        {
+            return left._size >= right._size;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator <=(VkDeviceSize left, VkDeviceSize right)
+        {
+            return left._size <= right._size;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator >(VkDeviceSize left, VkDeviceSize right)
+        {
+            return left._size > right._size;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator <(VkDeviceSize left, VkDeviceSize right)
+        {
+            return left._size < right._size;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VkDeviceSize operator +(VkDeviceSize left, VkDeviceSize right)
+        {
+            return (VkDeviceSize)(left._size + right._size);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static VkDeviceSize operator -(VkDeviceSize left, VkDeviceSize right)
+        {
+            return (VkDeviceSize)(left._size - right._size);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public bool Equals(in VkDeviceSize obj)
         {
             return obj._size == _size;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override bool Equals(object? obj)
         {
             return obj is VkDeviceSize vkDeviceSize && Equals(in vkDeviceSize);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override int GetHashCode()
         {
             return _size.GetHashCode();
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static implicit operator ulong(VkDeviceSize value)
         {
             return value._size;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator uint(VkDeviceSize value)
+        {
+            return (uint)value._size;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override string ToString()
         {
             return _size.ToString();
