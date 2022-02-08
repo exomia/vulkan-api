ec7ff4bbd476ef8f4edf50dad0701edec0afae39
DanielBaumert <daniel.baumert22@gmail.com>
2022-02-08 19:18:23 +0100

    add VulkanApiVersion13 to VkVersion

--- src/Exomia.Vulkan.Api.Core/Custom/VkVersion.cs
+++ src/Exomia.Vulkan.Api.Core/Custom/VkVersion.cs
@@ -32,6 +32,12 @@ public readonly struct VkVersion
     /// </summary>
     public static readonly VkVersion VulkanApiVersion12 = new VkVersion(0, 1, 2, 0);
 
+    /// <summary>
+    ///     The version 1.3
+    /// </summary>
+    public static readonly VkVersion VulkanApiVersion13 = new VkVersion(0, 1, 3, 0);
+
+
     private readonly uint _version;
 
     /// <summary>
