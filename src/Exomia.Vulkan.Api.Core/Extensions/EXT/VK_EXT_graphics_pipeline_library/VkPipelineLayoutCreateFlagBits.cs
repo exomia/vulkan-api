f15a4ba75a5a496639fea66cddbc60638db61b3a
Daniel Bätz <baetz.daniel@gmail.com>
2022-03-30 11:02:49 +0200

    rc v1.3.210.1

--- src/Exomia.Vulkan.Api.Core/Extensions/EXT/VK_EXT_graphics_pipeline_library/VkPipelineLayoutCreateFlagBits.cs
+++ src/Exomia.Vulkan.Api.Core/Extensions/EXT/VK_EXT_graphics_pipeline_library/VkPipelineLayoutCreateFlagBits.cs
@@ -13,9 +13,28 @@
 // ReSharper disable once CheckNamespace
 namespace Exomia.Vulkan.Api.Core;
 
+/// <summary>
+///     VkPipelineLayoutCreateFlagBits - Pipeline layout creation flag bits -
+///     <a href="https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VkPipelineLayoutCreateFlagBits.html">https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VkPipelineLayoutCreateFlagBits.html</a>
+/// </summary>
 [Flags]
 public enum VkPipelineLayoutCreateFlagBits
 {
-    VK_PIPELINE_LAYOUT_CREATE_RESERVED_0_BIT_AMD       = 0x1,
+    /// <summary>
+    ///     VK_PIPELINE_LAYOUT_CREATE_RESERVED_0_BIT_AMD<br />
+    ///     <a
+    ///         href="https://www.khronos.org/registry/vulkan/specs/1.3-extensions/html/vkspec.html#VkPipelineLayoutCreateFlagBits">
+    ///         https://www.khronos.org/registry/vulkan/specs/1.3-extensions/html/vkspec.html#VkPipelineLayoutCreateFlagBits
+    ///     </a>
+    /// </summary>
+    VK_PIPELINE_LAYOUT_CREATE_RESERVED_0_BIT_AMD = 0x1,
+
+    /// <summary>
+    ///     VK_PIPELINE_LAYOUT_CREATE_INDEPENDENT_SETS_BIT_EXT<br />
+    ///     <a
+    ///         href="https://www.khronos.org/registry/vulkan/specs/1.3-extensions/html/vkspec.html#VkPipelineLayoutCreateFlagBits">
+    ///         https://www.khronos.org/registry/vulkan/specs/1.3-extensions/html/vkspec.html#VkPipelineLayoutCreateFlagBits
+    ///     </a>
+    /// </summary>
     VK_PIPELINE_LAYOUT_CREATE_INDEPENDENT_SETS_BIT_EXT = 0x2
 }
