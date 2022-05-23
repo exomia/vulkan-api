f2c02f828108b94c9a377e7fb7e906ad63de0a17
Daniel Bätz <baetz.daniel@gmail.com>
2022-05-23 21:45:19 +0200

    removed constants from static vk -> they are now defined properly in the xml

--- src/Exomia.Vulkan.Api.Core/vk.cs
+++ src/Exomia.Vulkan.Api.Core/vk.cs
@@ -26,49 +26,10 @@ public static partial class Vk
     private const string VK_IMPORT = "vulkan";
 
     /// <summary>
-    ///     VK_MAX_PHYSICAL_DEVICE_NAME_SIZE - Length of a physical device name string -
-    ///     <a
-    ///         href="https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_MAX_PHYSICAL_DEVICE_NAME_SIZE.html">
-    ///         https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_MAX_PHYSICAL_DEVICE_NAME_SIZE.html
-    ///     </a>
-    /// </summary>
-    public const uint VK_MAX_PHYSICAL_DEVICE_NAME_SIZE = 256;
-
-    /// <summary>
-    ///     VK_UUID_SIZE - Length of a universally unique device or driver build identifier -
-    ///     <a href="https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_UUID_SIZE.html">https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_UUID_SIZE.html</a>
-    /// </summary>
-    public const uint VK_UUID_SIZE = 16;
-
-    /// <summary>
     ///     VK_LUID_SIZE_KHR - Length of a universally unique device or driver build identifier -
     ///     <a href="https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_LUID_SIZE_KHR.html">https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_LUID_SIZE_KHR.html</a>
     /// </summary>
     public const uint VK_LUID_SIZE_KHR = VK_LUID_SIZE;
-
-    /// <summary>
-    ///     VK_MAX_EXTENSION_NAME_SIZE - Maximum length of a layer of extension name string -
-    ///     <a href="https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_MAX_EXTENSION_NAME_SIZE.html">https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_MAX_EXTENSION_NAME_SIZE.html</a>
-    /// </summary>
-    public const uint VK_MAX_EXTENSION_NAME_SIZE = 256;
-
-    /// <summary>
-    ///     VK_MAX_DESCRIPTION_SIZE - Length of a driver name string -
-    ///     <a href="https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_MAX_DESCRIPTION_SIZE.html">https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_MAX_DESCRIPTION_SIZE.html</a>
-    /// </summary>
-    public const uint VK_MAX_DESCRIPTION_SIZE = 256;
-
-    /// <summary>
-    ///     VK_MAX_MEMORY_TYPES - Length of an array of memory types -
-    ///     <a href="https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_MAX_MEMORY_TYPES.html">https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_MAX_MEMORY_TYPES.html</a>
-    /// </summary>
-    public const uint VK_MAX_MEMORY_TYPES = 32;
-
-    /// <summary>
-    ///     VK_MAX_MEMORY_HEAPS - Length of an array of memory heaps -
-    ///     <a href="https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_MAX_MEMORY_HEAPS.html">https://www.khronos.org/registry/vulkan/specs/1.3-extensions/man/html/VK_MAX_MEMORY_HEAPS.html</a>
-    /// </summary>
-    public const uint VK_MAX_MEMORY_HEAPS = 16;
 
     /// <summary> Gets a vk function. </summary>
     /// <param name="vkInstance">           The vk instance. </param>
