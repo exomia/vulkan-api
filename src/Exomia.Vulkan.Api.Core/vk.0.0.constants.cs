dd757e4ebe61d9a2a84fe67d82eca1924a160eb4
Daniel Bätz <baetz.daniel@gmail.com>
2024-08-30 13:26:44 +0200

    Update vk.0.0.constants.cs

--- src/Exomia.Vulkan.Api.Core/vk.0.0.constants.cs
+++ src/Exomia.Vulkan.Api.Core/vk.0.0.constants.cs
@@ -15,7 +15,7 @@ namespace Exomia.Vulkan.Api.Core;
 /// <content>
 ///     VK_VERSION_0_0.constants - vulkan
 /// </content>
-public static class Vk
+public static partial class Vk
 {
     /// <summary>
     ///     VK_MAX_PIPELINE_BINARY_KEY_SIZE_KHR - Maximum length of binary key -
